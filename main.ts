dd0d2f9bdf243c61eb56608c026b9db335cdf97a
magava007 <90795959+magava007@users.noreply.github.com>
2021-09-15 14:08:06 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,73 @@
+basic.showLeds(`
+    . # . . .
+    . # . . .
+    # # . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . . . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    # # . . .
+    # . . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    # . . . .
+    . # . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(2000)
+basic.showLeds(`
+    # . . . .
+    # # . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    # # . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    # . . . .
+    # # . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    # . . . .
+    . # . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(5000)
+basic.showLeds(`
+    . # . . .
+    . # . . .
+    # # . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.forever(function () {
+	
+})
